b6bfc02d8f2d72ea2e56aa56d10de88828144d01
Drew Colgin <acolgin@schonfeld.com>
2025-08-29 23:01:03 -0700

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cutthroat-companies/', // base path for GitHub Pages project site
+  base: '/Cutthroat-Companies-Companion-App/', // base path for GitHub Pages project site
   plugins: [react()],
 })
